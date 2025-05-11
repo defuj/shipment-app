a88e77aac82d313cb1261d2b6afe860d2f290130
Dede Fuji Abdul <defuj.official@gmail.com>
2025-05-11 08:28:07 +0700

    feat: add main application structure with MaterialApp and search page integration

---
+++ lib/second_main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shipment/pages/search.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shipment App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        useMaterial3: true,
+      ),
+      home: const Search(),
+    );
+  }
+}
